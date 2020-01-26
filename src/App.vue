35255919ce5788acd81410a0c307ef16ffc8adfc
Non <nonmadden@gmail.com>
2020-01-26 11:08:11 +0700

    Add vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  <div id="booking" class="section">
+    <div class="section-center">
+      <div class="container">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
